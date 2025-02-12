a398bcdceb6a60c6024273149d1511211b0cd317
Madhav Arora <eragon.cube@outlook.com>
2025-02-12 18:34:53 +0530

    Refactor .env files

--- src/utils/axios-config.ts
+++ src/utils/axios-config.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:8000",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true
 });
 
